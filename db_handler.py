7375fdd1b19a53367d6d620bbc7acfd6a472b3d5
cleverman88 <sohaibkarous@gmail.com>
2021-02-15 11:58:47 +0000

    added database handler

---
+++ db_handler.py
@@ -0,0 +1,114 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################################
+#
+# (c) Copyright University of Southampton, 2021
+#
+# Copyright in this software belongs to University of Southampton,
+# Highfield, University Road, Southampton SO17 1BJ
+#
+# Created By : Stuart E. Middleton
+# Created Date : 2021/02/11
+# Project : Teaching
+#
+######################################################################
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import codecs, logging, sqlite3
+
+"""
+    Class to handle the database functions, designed for test guesses and real guesses
+"""
+
+
+class DBHandler:
+    """
+        Creates the logger and sets up the tables
+    """
+
+    def __init__(self):
+        LOG_FORMAT = ('%(levelname) -s %(asctime)s %(message)s')
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+        self.logger.info('logging started')
+        self.test_conn = sqlite3.connect('dbs/test_table.db')
+        self.real_conn = sqlite3.connect('dbs/real_table.db')
+        self.test_c = self.test_conn.cursor()
+        self.real_c = self.real_conn.cursor()
+
+    """
+        Reads a database given the name
+    """
+
+    def read_data(self, name):
+        self.logger.info("Reading data from database: " + name)
+        read_handle = codecs.open('csv/' + name + '', 'r', 'utf-8', errors='replace')
+        list_lines = read_handle.readlines()
+        read_handle.close()
+        return list_lines
+
+    """
+        Creates and sets up the test table 
+    """
+
+    def setup_test_table(self):
+        self.logger.info("Setting up test table")
+        self.test_c.execute(
+            'CREATE TABLE IF NOT EXISTS test_table (UserID INT, ItemID INT, Rating INT, PredRating FLOAT)')
+        self.test_conn.commit()
+        self.test_c.execute('DELETE FROM test_table')
+        self.test_conn.commit()
+
+    """
+        Creates and sets up the real table 
+    """
+
+    def setup_real_table(self):
+        self.logger.info("Setting up real table")
+        self.real_c.execute(
+            'CREATE TABLE IF NOT EXISTS real_table (UserID INT, ItemID INT, Rating INT)')
+        self.real_conn.commit()
+        self.real_c.execute('DELETE FROM real_table')
+        self.real_conn.commit()
+
+    """
+        Function for inserting into the real table
+    """
+
+    def insert_into_real_table(self, userid, itemid, predict):
+        self.test_c.execute('INSERT INTO test_table VALUES (?,?,?)',
+                            (userid, itemid, predict))
+
+    """
+        Function for inserting into the test table
+    """
+
+    def insert_into_test_table(self, userid, itemid, rating, predict):
+        self.test_c.execute('INSERT INTO test_table VALUES (?,?,?,?)',
+                            (userid, itemid, rating, predict))
+
+    """
+        Calculates the MSE for the test table
+    """
+
+    def calculate_test_mse(self):
+        # Creating an index first
+        self.test_conn.commit()
+        self.test_c.execute('CREATE INDEX IF NOT EXISTS test_table_index on test_table (UserID, ItemID)')
+        self.test_conn.commit()
+        # Calculating MSE
+        self.test_c.execute('SELECT AVG(ABS(Rating-PredRating)) FROM test_table WHERE PredRating IS NOT NULL')
+        row = self.test_c.fetchone()
+        nMSE = float(row[0])
+        self.logger.info('example MSE for prediction = ' + str(nMSE))
+
+    """
+        Closes all the connections
+    """
+    def end(self):
+        self.test_c.close()
+        self.test_conn.close()
+        self.real_c.close()
+        self.real_conn.close()
---
+++ main.py
@@ -0,0 +1,94 @@
+from db_handler import DBHandler
+"""
+Ignore functions here just used for testing
+"""
+
+def similarity(person_a, person_b, both_reviewed):
+	average_of_a = get_average(person_a)
+	average_of_b = get_average(person_b)
+
+	import math
+	top = 0
+	bot1 = 0
+	bot2 = 0
+	for item in both_reviewed:
+		top += (person_a[item] - average_of_a) * (person_b[item] - average_of_b)
+		bot1 += (person_a[item] - average_of_a) ** 2
+		bot2 += (person_b[item] - average_of_b) ** 2
+
+	if bot1 == 0 or bot2 == 0:
+		return 0
+	pearson = top / (math.sqrt(bot1) * math.sqrt(bot2))
+	return pearson
+
+
+def create_neighbourhood(user_info,db):
+	print(len(user_info))
+	for person_a, info in user_info.items():
+		neighbour = {}
+		all_items = set([k for k, v in info.items()])
+		print("Predicting for " + person_a)
+		for person_b, i2 in user_info.items():
+			if person_b == person_a:
+				continue
+			both_reviewed = get_both_reviewed(user_info[person_a], user_info[person_b])
+
+			if similarity(user_info[person_a], user_info[person_b], both_reviewed) > 0.5:
+				neighbour[person_b] = i2
+				for x in [k for k, v in i2.items()]:
+					if x in all_items:
+						all_items.remove(x)
+
+			if len(neighbour) < 5 and len(all_items) == 0:
+				break
+
+		predict(person_a,user_info[person_a], neighbour,db)
+
+	db.calculate_test_mse()
+
+
+def get_both_reviewed(person_a, person_b):
+	both_reviewed = []
+	for k, v in person_a.items():
+		if k in person_b:
+			both_reviewed.append(k)
+	return both_reviewed
+
+
+def get_average(person_a):
+	return sum([float(v) for k, v in person_a.items()]) / len(person_a.items())
+
+
+def predict(userid, user, neighbourhood,db):
+	average_of_user = get_average(user)
+	predict_items = [k for k, v in user.items()]
+	top = 0
+	bot = 0
+	for item in predict_items:
+		for k, v in neighbourhood.items():
+			if item not in v:
+				continue
+			top += similarity(user, v, get_both_reviewed(v, user)) * (v[item] - get_average(v))
+			bot += similarity(user, v, get_both_reviewed(v, user))
+
+		pred = average_of_user + (top / bot)
+		db.insert_into_test_table(userid,item,user[item],pred)
+
+
+if __name__ == '__main__':
+	db = DBHandler()
+	db.setup_test_table()
+
+	user_info = {}
+	for x in db.read_data("comp3208-train-small.csv"):
+		listParts = x.strip().split(',')
+		if listParts[2] == "rating":
+			continue
+		if listParts[0] in user_info:
+			user_info[listParts[0]][listParts[1]] = float(listParts[2])
+		else:
+			user_info[listParts[0]] = {listParts[1]: float(listParts[2])}
+
+	print(similarity(user_info["1"], user_info["2"], get_both_reviewed(user_info["1"], user_info["2"])))
+	print(user_info["6"])
+	create_neighbourhood(user_info,db)
